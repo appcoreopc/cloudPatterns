395d752c28cd90bb2d253559145bd0dab9dff796
jeremy woo <kepungmoin@hotmail.com>
2019-04-01 21:57:19 +1300

    remove key

--- competingconsumer/ServiceQueueFunction/ConsumerFuncApp.cs
+++ competingconsumer/ServiceQueueFunction/ConsumerFuncApp.cs
@@ -8,7 +8,7 @@ namespace ServiceQueueFunction
     public static class ConsumerFuncApp
     {
         [FunctionName("ConsumerFuncApp")]
-        public static void Run([ServiceBusTrigger("tradequeue", Connection = "Endpoint=sb://sbservice550.servicebus.windows.net/;SharedAccessKeyName=accesstradequeue;SharedAccessKey=p1TJpen9vgB7RthWBwNB7OrnV3/rwW3FhlyQ9/2K54g=;")]string myQueueItem, ILogger log)
+        public static void Run([ServiceBusTrigger("tradequeue", Connection = "")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
         }
