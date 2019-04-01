f5ba8798908a0956ef72636776685ebd50861f00
jeremy woo <kepungmoin@hotmail.com>
2019-04-01 22:11:09 +1300

    azure function to fetch queue data.

--- competingconsumer/ServiceQueueFunction/ConsumerFuncApp.cs
+++ competingconsumer/ServiceQueueFunction/ConsumerFuncApp.cs
@@ -8,7 +8,7 @@ namespace ServiceQueueFunction
     public static class ConsumerFuncApp
     {
         [FunctionName("ConsumerFuncApp")]
-        public static void Run([ServiceBusTrigger("tradequeue", Connection = "")]string myQueueItem, ILogger log)
+        public static void Run([ServiceBusTrigger("tradequeue", Connection = "AzureWebJobsStorage")]string myQueueItem, ILogger log)
         {
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
         }
